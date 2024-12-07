650e8191b7edf226c6310cf031d5fe6be1f08f3a
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-12-08 01:38:21 +0500

    removed

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import LayoutPage from "./pages/LayoutPage";
-// import DetailedContact, { userDetailsLoader } from "./pages/DetailedContact";
 import MainPage from "./pages/MainPage";
-// import Contacts, { usersLoader } from "./pages/Contacts";
-// import About from "./pages/About";
-// import UserNotFound from "./pages/UserNotFound";
  
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,11 +13,8 @@ const router = createBrowserRouter(
 
       <Route index={true} element={<MainPage />} />
 
-      {/* <Route path="contacts" element={<Contacts />} loader={usersLoader} /> */}
 
-      {/* <Route path="contacts/:contactID" element={<DetailedContact />} loader={userDetailsLoader} errorElement={<UserNotFound/>}/> */}
 
-      {/* <Route path="about" element={<About />} /> */}
 
     </Route>
   )
@@ -29,30 +22,4 @@ const router = createBrowserRouter(
 
 export default router;
 
-// "Component" jab use karen ge to jsx use nhi karen ge
-// "Element" bhi use kar sakte hen
-// using "index={true}" is a better way to define home page as compared to " path="" "
 
-
-// hum ese bhi use kar sakte hen
-// const router = createBrowserRouter([{
-//     path:'',
-//     Component: LayoutPage,
-//     errorElement: <NotFound/>,
-//     children:[
-//         {
-//             path: '',
-//             Component: MainPage
-//         }
-//         ,
-//         {
-//             path: 'contacts',
-//             Component: Contacts
-//         }
-//         ,
-//         {
-//             path: 'contacts/:contactID',
-//             Component: DetailedContact
-//         }
-//     ]
-// }])
